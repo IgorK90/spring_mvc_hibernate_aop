ab3f21da4a0e45ed97d8110bd29059e563c75f5e
Home <igor.kiyashko@inbox.ru>
2022-09-08 20:03:40 +0600

    spring_mvc_hibernate_aop

---
+++ src/main/java/com/zaurtregulov/spring/mvc_hibernate_aop/dao/EmployeeDAOImpl.java
@@ -0,0 +1,16 @@
+package com.zaurtregulov.spring.mvc_hibernate_aop.dao;
+
+import com.zaurtregulov.spring.mvc_hibernate_aop.entity.Employee;
+
+import java.util.List;
+
+
+public class EmployeeDAOImpl implements EmployeeDAO {
+
+
+
+    @Override
+    public List<Employee> getAllEmployees() {
+        return null;
+    }
+}
